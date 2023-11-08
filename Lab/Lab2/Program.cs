21cacec93253320c353980c9fb0eb0a740b646a6
dkontek <dkontek@RA117A-01>
2023-11-08 12:33:14 +0100

    lab zad 1

---
+++ Lab/Lab2/Program.cs
@@ -0,0 +1,26 @@
+﻿using Lab2.Models;
+using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var licz = new Licz();
+
+
+            licz.DisplayValue();
+
+            licz.Add(5);
+
+            licz.DisplayValue();
+
+            licz.Add(7);
+
+            licz.Substract(2);
+
+            licz.DisplayValue();
+        }
+    }
+}
